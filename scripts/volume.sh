b312b29a2f02a8f217267247aa941090c0a47a77
Alejandro H Tadino M <tadinomalejandroh@gmail.com>
2023-08-30 07:21:33 -0400

    [INIT] Initial commit

---
+++ scripts/volume.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if eww windows | rg -q "\*volume"; then
+    eww update volume-level="$(wpctl get-volume @DEFAULT_AUDIO_SINK@)"
+    eww update volume-hidden=false
+else
+    eww close brightness
+    eww open volume
+
+    eww update volume-level="$(wpctl get-volume @DEFAULT_AUDIO_SINK@)"
+    eww update volume-hidden=false
+    sleep 2
+    eww update volume-hidden=true
+    sleep 1
+    eww close volume
+fi
